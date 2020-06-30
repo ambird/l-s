f7e935ce89d1802ed2f2ffa8ad3f10be2cac6ea6
wxchangshengwei <1156230224@qq.com>
2020-06-30 15:06:42 +0800

    1.搭建Eureka注册中心 2.搭建Eureka客户端

---
+++ eureka-client/src/main/java/com/cf/cloud/EurekaClientApplication.java
@@ -0,0 +1,21 @@
+package com.cf.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author 常飞
+ * @date 2020-6-30
+ */
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
